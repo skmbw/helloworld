8434bf987b4ea2147b59d626f0f3d91b31ddc642
skmbw <tongku2008@126.com>
2017-08-03 15:20:28 +0800

    封装一下重复性的代码

---
+++ app/src/main/java/com/fuuzii/grpc/GenericGrpcTask.java
@@ -0,0 +1,59 @@
+package com.fuuzii.grpc;
+
+import android.os.AsyncTask;
+
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+/**
+ * @author yinlei
+ * @since 2017/8/3 14:51
+ */
+public abstract class GenericGrpcTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+    private String mHost;
+    private int mPort;
+    protected ManagedChannel mChannel;
+
+    public GenericGrpcTask() {
+        mHost = "10.70.7.244";
+        mPort = 50015;
+    }
+
+    public GenericGrpcTask(String host, int port) {
+        mHost = host;
+        mPort = port;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        preExecute();
+    }
+
+    public abstract void preExecute();
+
+    @Override
+    protected Result doInBackground(Params... params) {
+        mChannel = ManagedChannelBuilder.forAddress(mHost, mPort)
+                .usePlaintext(true)
+                .build();
+        return background(params);
+    }
+
+    public abstract Result background(Params... params);
+
+    @Override
+    protected void onPostExecute(Result result) {
+        try {
+            mChannel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        postExecute(result);
+    }
+
+    public abstract void postExecute(Result result);
+
+
+}
